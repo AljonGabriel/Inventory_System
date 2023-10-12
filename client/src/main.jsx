d25a2492bc34f43b4e759d2bf2ea3e21235017c6
Aljonator <121176500+AljonGabriel@users.noreply.github.com>
2023-10-12 18:53:07 +0800

    Trying to deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename='/Inventory_System/client/'>
+    <Router basename='/client/'>
       <App />
     </Router>
   </React.StrictMode>,
