c2e6f218d29c386a67680f182cdb9d04d009dbd9
Aljonator <121176500+AljonGabriel@users.noreply.github.com>
2023-10-31 23:45:13 +0800

    Creating Graph using Libraries

---
+++ frontend/src/components/graphs/ItemsGraph.jsx
@@ -0,0 +1,9 @@
+const ItemsGraph = () => {
+  return (
+    <>
+      <h2>ItemsGraph</h2>
+    </>
+  );
+};
+
+export default ItemsGraph;
--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
+import ItemsGraph from "../../components/graphs/ItemsGraph";
 import ItemsTable from "../../components/itemTable/ItemsTable";
 function Dashboard() {
   return (
     <>
+      <ItemsGraph />
       <ItemsTable />
     </>
   );
